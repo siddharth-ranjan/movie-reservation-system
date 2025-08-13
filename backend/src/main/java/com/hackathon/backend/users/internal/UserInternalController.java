dbd21a0a650cb1fea5ad16599ef0f0ba2d2b93b6
Siddharth Ranjan <siddharthranjan0909@gmail.com>
2025-08-13 12:11:12 +0530

    chore: add functional endpoints

--- backend/src/main/java/com/hackathon/backend/users/internal/UserInternalController.java
+++ backend/src/main/java/com/hackathon/backend/users/internal/UserInternalController.java
@@ -2,32 +2,33 @@ package com.hackathon.backend.users.internal;
 
 import com.hackathon.backend.users.User;
 import com.hackathon.backend.users.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.hackathon.backend.users.dto.RegisterRequest;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
 class UserInternalController {
 
-    @Autowired
     private final UserService userService;
 
     public UserInternalController(UserService userService) {
         this.userService = userService;
     }
 
-    @PostMapping("/save")
-    User save(@RequestBody User user) {
-        return userService.save(user);
+    @PostMapping("/register")
+    User save(@RequestBody RegisterRequest user) {
+        return userService.register(user);
     }
 
-    @GetMapping("/{id}")
-    User findById(@PathVariable String id) {
-        return userService.findUser();
+    @GetMapping("/admin/users")
+    List<User> findAll() {
+        return userService.findAll();
     }
 
-
+    @PostMapping("/admin/user/{id}")
+    User findById(@PathVariable Long id) {
+        return userService.findById(id);
+    }
 
 }
