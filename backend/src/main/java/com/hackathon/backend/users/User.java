317662208b81b3c3fcb371fb9abc624111700309
Siddharth Ranjan <siddharthranjan0909@gmail.com>
2025-08-13 12:13:29 +0530

    fix: password store

--- backend/src/main/java/com/hackathon/backend/users/User.java
+++ backend/src/main/java/com/hackathon/backend/users/User.java
@@ -2,6 +2,9 @@ package com.hackathon.backend.users;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+
+@Table(name = "app_user")
 @Entity
 public class User {
 
@@ -10,6 +13,67 @@ public class User {
     private Long id;
 
     private String username;
-//    private String password;
+    private String password;
     private String email;
+    private LocalDate birthDate;
+
+    public User() {
+    }
+
+    public User(String username, String password, String email, LocalDate birthDate) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
