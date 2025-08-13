06905bd77f1a8b45f4fb46ded3e289eea65fe185
Siddharth Ranjan <siddharthranjan0909@gmail.com>
2025-08-13 12:10:17 +0530

    chore: bypass auth for development stage

--- backend/src/main/java/com/hackathon/backend/users/internal/security/SecurityConfig.java
+++ backend/src/main/java/com/hackathon/backend/users/internal/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -15,7 +16,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
+//                          .requestMatchers("/register").permitAll()
 //                        // Permit access to public endpoints without authentication
 //                        .requestMatchers("/api/public/**", "/login").permitAll()
 //                        // Require ADMIN role for admin endpoints
@@ -23,9 +26,9 @@ public class SecurityConfig {
 //                        .requestMatchers("/api/user/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
                         // All other requests must be authenticated
                         .anyRequest().permitAll()
-                )
+                );
                 // Use form login for authentication
-                .formLogin(Customizer.withDefaults());
+//                .formLogin(Customizer.withDefaults());
 
         return http.build();
 
