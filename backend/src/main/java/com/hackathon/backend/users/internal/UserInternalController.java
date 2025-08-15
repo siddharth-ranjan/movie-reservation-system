8422ca40e2bee4d36ffbdb97bee8ea379c7048a4
Siddharth Ranjan <siddharthranjan0909@gmail.com>
2025-08-15 19:07:13 +0530

    restructure: endpoints

--- backend/src/main/java/com/hackathon/backend/users/internal/UserInternalController.java
+++ backend/src/main/java/com/hackathon/backend/users/internal/UserInternalController.java
@@ -1,34 +1,51 @@
 package com.hackathon.backend.users.internal;
 
-import com.hackathon.backend.users.User;
 import com.hackathon.backend.users.UserService;
+import com.hackathon.backend.users.dto.LoginRequest;
 import com.hackathon.backend.users.dto.RegisterRequest;
+import com.hackathon.backend.users.internal.security.JwtTokenProvider;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-class UserInternalController {
+@RequestMapping("/api/users")
+public class UserInternalController {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public UserInternalController(UserService userService) {
+    public UserInternalController(UserService userService, AuthenticationManager authenticationManager, UserDetailsService userDetailsService, JwtTokenProvider jwtTokenProvider) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.userDetailsService = userDetailsService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
-    @PostMapping("/register")
-    User save(@RequestBody RegisterRequest user) {
-        return userService.register(user);
+    @PostMapping("/register") // Changed from /create
+    public ResponseEntity<String> register(@RequestBody RegisterRequest registerRequest) {
+        try {
+            userService.registerUser(registerRequest);
+            return ResponseEntity.ok("User registered successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().
+                    body("User already registered. Please login");
+        }
+
     }
 
-    @GetMapping("/admin/users")
-    List<User> findAll() {
-        return userService.findAll();
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.username(), loginRequest.password())
+        );
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequest.username());
+        final String jwt = jwtTokenProvider.generateToken(userDetails);
+        return ResponseEntity.ok(jwt);
     }
-
-    @GetMapping("/admin/user/{id}")
-    User findById(@PathVariable Long id) {
-        return userService.findById(id);
-    }
-
 }
