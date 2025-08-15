0a28fdf5dc7bba67e4979de4e1300dc8711e4405
Siddharth Ranjan <siddharthranjan0909@gmail.com>
2025-08-15 19:09:51 +0530

    chore: implement userDetails

--- backend/src/main/java/com/hackathon/backend/users/User.java
+++ backend/src/main/java/com/hackathon/backend/users/User.java
@@ -1,44 +1,77 @@
 package com.hackathon.backend.users;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.hackathon.backend.users.dto.Role;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 @Table(name = "app_user")
 @Entity
-public class User {
+public class User implements UserDetails {
 
-    @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
-    @Column(name = "user_name", nullable = false, unique = true)
+    @Column(name = "user_name", unique = true, nullable = false)
     private String username;
 
-    @Column(name = "user_password", nullable = false)
-    @JsonIgnore
-    private String password;
-
-    @Column(name = "user_email", nullable = false, unique = true)
+    @Column(name = "user_email", unique = true, nullable = false)
     private String email;
 
-    @JsonIgnore
-    @Column(name = "user_birthdate", nullable = false)
+    @Column(name = "user_password", nullable = false)
+    private String password;
+
+    @Column(name = "user_birthdate")
     private LocalDate birthDate;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
 
     public User() {
     }
 
-    public User(String username, String password, String email, LocalDate birthDate) {
+    public User(String username, String password, String email, LocalDate birthDate,  Role role) {
         this.username = username;
         this.password = password;
         this.email = email;
         this.birthDate = birthDate;
+        Role userRole = Role.valueOf(role.name());
     }
 
     public Long getId() {
         return id;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
     }
 
     public void setId(Long id) {
